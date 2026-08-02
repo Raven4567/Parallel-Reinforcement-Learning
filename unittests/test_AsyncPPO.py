e3122a899cee07ca19551109beb323004678b4ca
Raven4567 <aidencaldwell42@gmail.com>
2026-08-03 01:43:49 +0300

    Switch from unittest to pytest.

--- unittests/test_AsyncPPO.py
+++ unittests/test_AsyncPPO.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 
 import os
 import sys
@@ -12,64 +12,91 @@ import gymnasium as gym
 
 import numpy as np
 
-class TestVecMemory(unittest.TestCase):
-    def setUp(self):
-        # Dummy data
-        self.state = np.random.randn(1)
-        self.action = np.random.randint(0, 2, size=(1,))
-        self.reward = np.random.rand(1)
-        self.done = np.random.choice([True, False])
-        # self.state_value = np.random.randn(4)
-        # self.log_prob = np.random.rand()
+class TestVecMemory:
+    @pytest.fixture
+    def vec_memory_setup(self):
+        memory = AsyncTools.AsyncPPO.VecMemory(num_envs=4)
     
-        self.memory = AsyncTools.AsyncPPO.VecMemory(num_envs=4)
+        # Dummy data
+        state = np.random.randn(1)
+        action = np.random.randint(0, 2, size=(1,))
+        reward = np.random.rand(1)
+        done = np.random.choice([True, False], size=(1,))
+        # state_value = np.random.randn(4)
+        # log_prob = np.random.rand()
+        
+        return {
+            "memory": memory,
+            "state": state,
+            "action": action,
+            "reward": reward,
+            "done": done
+            # "state_value": state_value,
+            # "log_prob": log_prob
+        }
+    
+    def test_push(self, vec_memory_setup):
+        memory = vec_memory_setup["memory"]
+        
+        state = vec_memory_setup["state"]
+        action = vec_memory_setup["action"]
+        reward = vec_memory_setup["reward"]
+        done = vec_memory_setup["done"]
+        # state_value = vec_memory_setup["state_value"]
+        # log_prob = vec_memory_setup["log_prob"]
 
-    def test_push(self):
-        self.memory.push(
+        memory.push(
             idx=0,
-            state=self.state,
-            action=self.action,
-            reward=self.reward,
-            done=self.done,
+            state=state,
+            action=action,
+            reward=reward,
+            done=done,
             # state_value=self.state_value,
             # log_prob=self.log_prob
         )
     
-    def test_clear(self):
-        self.memory.clear()
+    def test_clear(self, vec_memory_setup):
+        memory = vec_memory_setup["memory"]
+        memory.clear()
 
-class TestEnvVectorizer(unittest.TestCase):
-    def setUp(self):
-        self.env = gym.make('CartPole-v1')
-        self.env_vectorizer = AsyncTools.AsyncPPO.EnvVectorizer(env=self.env, num_envs=4)
-    
-    def test_reset(self):
-        self.env_vectorizer.reset()
-    
-    def test_step(self):
-        self.env_vectorizer.reset()
+class TestEnvVectorizer:
+    @pytest.fixture
+    def setup_env_vectorizer(self):
+        env = gym.make('CartPole-v1')
+        env_vectorizer = AsyncTools.AsyncPPO.EnvVectorizer(env, num_envs=4)
         
-        self.env_vectorizer.step(actions=np.random.randint(0, 2, size=4))
+        return env_vectorizer
+    
+    def test_reset(self, setup_env_vectorizer):
+        env_vectorizer = setup_env_vectorizer
+        env_vectorizer.reset()
+    
+    def test_step(self, setup_env_vectorizer):
+        env_vectorizer = setup_env_vectorizer
+        env_vectorizer.reset()
+        env_vectorizer.step(actions=np.random.randint(0, 2, size=4))
 
-class TestAsyncPPO(unittest.TestCase):
-    def setUp(self):
-        self.env = gym.make('CartPole-v1')
-        self.model = PPO(
+class TestAsyncPPO:
+    @pytest.fixture
+    def setup_async_ppo(self):
+        env = gym.make('CartPole-v1')
+        model = PPO(
             is_continuous=False, action_dim=2, observ_dim=4
         )
 
-        self.async_ppo = AsyncTools.AsyncPPO.AsyncPPO(
-            env=self.env,
-            ppo=self.model,
+        async_ppo = AsyncTools.AsyncPPO.AsyncPPO(
+            env=env,
+            ppo=model,
             num_envs=4,
             steps=1000
         )
+        
+        return async_ppo
     
-    def test_worker(self):
-        self.async_ppo.worker()
+    def test_worker(self, setup_async_ppo):
+        async_ppo = setup_async_ppo
+        async_ppo.worker()
 
-    def test_run(self):
-        self.async_ppo.run()
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_run(self, setup_async_ppo):
+        async_ppo = setup_async_ppo
+        async_ppo.run()
